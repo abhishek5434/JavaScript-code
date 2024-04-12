542315b624f453ce66f67458b6f73ea5eadc9aae
Abhishek Raj <86175919+abhishek5434@users.noreply.github.com>
2024-04-12 11:55:57 +0530

    Create 02_datatype.js

---
+++ 02_datatype.js
@@ -0,0 +1,34 @@
+"use strict"; //Treat all code as newer version
+
+// alert(3 + 3) //This doesn't work in node js, only works in browser
+
+ let name = "Abhishek"
+ let age =18
+ let islogged =true
+
+ //number ==> 2 to power 53
+ //bigint
+ //String can be defined in single or double quote
+ //boolean true or false
+ //null
+ //undefined
+ //symbol  ==> uniqueness 
+
+ //object
+
+//  console.log(typeof undefined) //undefined
+//  console.log(typeof null); //object
+
+let number = "33"
+console.log(number)
+console.log(typeof(number))
+
+let valueInNumber = Number(number)
+console.log(valueInNumber)
+console.log(typeof(valueInNumber))
+
+//"33" --> can be converted to number
+//"33abc" --> can be converted but the value will result in NaN(Not a number)
+// true = 1; false = 0
+
+
